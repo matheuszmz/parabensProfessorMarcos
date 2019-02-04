2b51d119608ef903a657cf1a7af78e9c37cc3b71
Matheus de A. Santana <matheuszmz@icloud.com>
2019-02-03 22:57:48 -0200

    Update parabens.py

--- parabens.py
+++ parabens.py
@@ -9,25 +9,25 @@ while True:
     op = input('Informe um número de 0-9: ')
 
     if op == '0':
-        print('Este é o dia em que o senhor agiu; alegremo-nos e exultemos neste dia. (Salmo 118.24)\n\n')
+        print('\n\nEste é o dia em que o senhor agiu; alegremo-nos e exultemos neste dia. (Salmo 118.24)\n\n')
     elif op == '1':
-        print('Feliz aniversário! Que em seu dia especial você possa sentir a presença de Deus te inspirando, te envolvendo com todo amor e proteção de sempre. Que surpresas grandes e boas se revelem e que tudo o que aconteça hoje te faça sorrir como nunca, provocando a verdadeira felicidade!\n\n')
+        print('\n\nFeliz aniversário! Que em seu dia especial você possa sentir a presença de Deus te inspirando, te envolvendo com todo amor e proteção de sempre. Que surpresas grandes e boas se revelem e que tudo o que aconteça hoje te faça sorrir como nunca, provocando a verdadeira felicidade!\n\n')
     elif op == '2':
-        print('Feliz Aniversário! Que a vida continue te surpreendendo, ensinando, te fazendo rir de tudo que já passou e orgulhoso de quem se tornou!\n\n')
+        print('\n\nFeliz Aniversário! Que a vida continue te surpreendendo, ensinando, te fazendo rir de tudo que já passou e orgulhoso de quem se tornou!\n\n')
     elif op == '3':
-        print('Feliz aniversário! A vida é um presente e deve ser usada para alcançar a glória divina. Hoje é dia de agradecer pelas bênçãos e por todo o aprendizado adquirido até aqui e reafirmar o seu compromisso com o Senhor, fortalecendo ainda mais a sua fé e continuando a seguir no caminho da luz e do bem.\n\n')
+        print('\n\nFeliz aniversário! A vida é um presente e deve ser usada para alcançar a glória divina. Hoje é dia de agradecer pelas bênçãos e por todo o aprendizado adquirido até aqui e reafirmar o seu compromisso com o Senhor, fortalecendo ainda mais a sua fé e continuando a seguir no caminho da luz e do bem.\n\n')
     elif op == '4':
-        print('Feliz aniversário! Que em seu dia especial você possa sentir a presença de Deus te inspirando, te envolvendo com todo amor e proteção de sempre. Que surpresas grandes e boas se revelem e que tudo o que aconteça hoje te faça sorrir como nunca, provocando a verdadeira felicidade!\n\n')
+        print('\n\nFeliz aniversário! Que em seu dia especial você possa sentir a presença de Deus te inspirando, te envolvendo com todo amor e proteção de sempre. Que surpresas grandes e boas se revelem e que tudo o que aconteça hoje te faça sorrir como nunca, provocando a verdadeira felicidade!\n\n')
     elif op == '5':
-        print('Que o dia de hoje seja cheio de lembranças felizes do passado e grandes esperanças para o futuro. Feliz Aniversário!\n\n')
+        print('\n\nQue o dia de hoje seja cheio de lembranças felizes do passado e grandes esperanças para o futuro. Feliz Aniversário!\n\n')
     elif op == '6':
-        print('Feliz Aniversário! Que todos os seus sonhos sejam realizados e todos os seus desejos alcançados! Que a paz Reine em seu coração! Felicidades!\n\n')
+        print('\n\nFeliz Aniversário! Que todos os seus sonhos sejam realizados e todos os seus desejos alcançados! Que a paz Reine em seu coração! Felicidades!\n\n')
     elif op == '7':
-        print('Te desejo uma vida cheia de boas surpresas, uma alma cheia de leveza e a felicidade transbordando pelo sorriso aberto que você insiste em carregar. Muito Sucesso! Parabéns para Você!\n\n')
+        print('\n\nTe desejo uma vida cheia de boas surpresas, uma alma cheia de leveza e a felicidade transbordando pelo sorriso aberto que você insiste em carregar. Muito Sucesso! Parabéns para Você!\n\n')
     elif op == '8':
-        print('Muitos anos de vida, para que sua estrela continue a brilhar com muita intensidade, você merece muito mais! Te desejo muitas bençãos! Feliz Aniversário!\n\n')
+        print('\n\nMuitos anos de vida, para que sua estrela continue a brilhar com muita intensidade, você merece muito mais! Te desejo muitas bençãos! Feliz Aniversário!\n\n')
     elif op == '9':
-        print('Feliz aniversário! É tempo de agradecer ao Senhor pela vida valiosa que Ele te concedeu. Que a esperança e a fé se renovem em ti, que não faltem razões para sorrir e agradecer a cada novo dia que começar. Muitas bênçãos, alegrias e otimismo para entrever o futuro lindo que te aguarda!\n\n')
+        print('\n\nFeliz aniversário! É tempo de agradecer ao Senhor pela vida valiosa que Ele te concedeu. Que a esperança e a fé se renovem em ti, que não faltem razões para sorrir e agradecer a cada novo dia que começar. Muitas bênçãos, alegrias e otimismo para entrever o futuro lindo que te aguarda!\n\n')
     elif op == 'x' or op == 'X':
         break
     else:
